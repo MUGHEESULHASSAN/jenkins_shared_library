ecd30a91f012f2a82ecdc6603cd33f10e81f9422
Muhammad Mughees Ul Hassan <130926299+MUGHEESULHASSAN@users.noreply.github.com>
2025-07-29 03:50:57 +0500

    Update clone.groovy

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,4 +1,4 @@
-def call(String url,String branch1){
+def call(String url,String branch){
   echo '📦 Cloning Django app...'
   git branch: "${branch}", url: "${url}"
   echo "code clonning successfully"
